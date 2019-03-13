1210343c4fef93421bce945bf1bd28b5ebe63622
noothankrishnan <44685235+noothankrishnan@users.noreply.github.com>
2019-03-13 13:16:22 -0500

    Update jsontotable.js

--- jsontotable.js
+++ jsontotable.js
@@ -37,9 +37,10 @@ function getData(url, mode=null) {
         } else {
             if (this.status != 200) {
                 console.log('Error Code :', this.status);
+                document.querySelector(".load-icon").classList.remove('lds-hourglass');
+                document.querySelector('.showdata').innerHTML = "";
             }
-            document.querySelector(".load-icon").classList.remove('lds-hourglass');
-            document.querySelector('.showdata').innerHTML = "";
+            
         }
 
     }
